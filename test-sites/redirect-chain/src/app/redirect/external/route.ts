b7b941f6f6491899a9e6cafb00890f9fb0b85290
박민수 <68321360+minsoo0506@users.noreply.github.com>
2026-05-31 14:54:37 +0900

    [Chore] test-sites 디렉터리 제거
    Refs #33

--- test-sites/redirect-chain/src/app/redirect/external/route.ts
+++
@@ -1,7 +0,0 @@
-import { EXTERNAL_TEST_URL, redirectResponse } from "@/lib/redirects";
-
-export async function GET() {
-  return await redirectResponse(EXTERNAL_TEST_URL);
-}
-
-export const HEAD = GET;
